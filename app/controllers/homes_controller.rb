20d9d1256ced06f6b81804e72a9cf34f2c89977a
yuuichi-komaki <yuuichi.k.1109@gmail.com>
2021-05-16 00:53:20 +0000

    修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def top
-    @posts = Post.all.includes(:user, :course, :post_images).order(created_at: :desc).page(params[:page]).per(9)
+    @posts = Post.all.includes(:user, :course, :post_images).order(created_at: :desc).page(params[:page]).per(6)
   end
 end
